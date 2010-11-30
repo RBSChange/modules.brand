cf9e209ce855da343f41e9e5ced04c68d6360eef
Eric HAUSWALD <eric.hauswald@rbs.fr>
2010-11-30 10:31:29 +0100

    [FIX] Correction patch 0350

--- patch/0350/install.php
+++ patch/0350/install.php
@@ -5,26 +5,14 @@
  */
 class brand_patch_0350 extends patch_BasePatch
 {
-//  by default, isCodePatch() returns false.
-//  decomment the following if your patch modify code instead of the database structure or content.
-    /**
-     * Returns true if the patch modify code that is versionned.
-     * If your patch modify code that is versionned AND database structure or content,
-     * you must split it into two different patches.
-     * @return Boolean true if the patch modify code that is versionned.
-     */
-//	public function isCodePatch()
-//	{
-//		return true;
-//	}
  
 	/**
 	 * Entry point of the patch execution.
 	 */
 	public function execute()
 	{
-		$task = task_PlannedtaskService::getInstance()->getBySystemtaskclassname('brand_BackgroundCompileTask');
-		if ($task !== null)
+		$tasks = task_PlannedtaskService::getInstance()->getBySystemtaskclassname('brand_BackgroundCompileTask');
+		foreach ($tasks as $task) 
 		{
 			$task->delete();
 		}
