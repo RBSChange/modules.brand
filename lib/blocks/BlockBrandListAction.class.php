ba5daf3bfe1ab1084e600d151d655ddf5bbb4f59
Gaël PORT <gael.port@rbs.fr>
2011-07-07 12:40:05 +0200

    [NEW] Styles and templates homogenization.

--- lib/blocks/BlockBrandListAction.class.php
+++ lib/blocks/BlockBrandListAction.class.php
@@ -16,26 +16,29 @@ class brand_BlockBrandListAction extends website_BlockAction
 		
 		$bs = brand_BrandService::getInstance();
 		$brandsByFirstLetter = array();
+		$topShelf = $this->getConfiguration()->getTopshelf();
 		if ($this->getConfiguration()->getPaginated())
 		{
 			$letter = $request->getParameter('letter', 'A');
-			$brands = $bs->getPublishedByWebsite($website, $letter, $this->getConfiguration()->getTopshelf());
+			$brands = $bs->getPublishedByWebsite($website, $letter, $topShelf);
 			$maxresults = $this->getConfiguration()->getItemsPerPage();
 			$page = $request->getParameter(paginator_Paginator::PAGEINDEX_PARAMETER_NAME, 1);
+			if (!is_numeric($page) || $page < 1 || $page > ceil(count($brands) / $maxresults))
+			{
+				$page = 1;
+			}
 			$paginator = new paginator_Paginator('brand', $page, $brands, $maxresults);
 			$brandsByFirstLetter[$letter] = $paginator;
-			$request->setAttribute('firstLetters', $bs->getFirstLettersByWebsite($website));
+			$request->setAttribute('firstLetters', $bs->getFirstLettersByWebsite($website, $topShelf));
+			$this->getContext()->addCanonicalParam('letter', $letter, $this->getModuleName());
+			$this->getContext()->addCanonicalParam('page', $page > 1 ? $page : null, $this->getModuleName());
 		}
 		else
 		{
-			$brands = $bs->getPublishedByWebsite($website, null, $this->getConfiguration()->getTopshelf());
+			$brands = $bs->getPublishedByWebsite($website, null, $topShelf);
 			foreach ($brands as $brand)
 			{
 				$firstLetter = $brand->getFirstLetter();
-				if (!isset($firstLetter))
-				{
-					$brandsByFirstLetter[$firstLetter] = array();
-				}
 				$brandsByFirstLetter[$firstLetter][] = $brand;
 			}
 			$request->setAttribute('firstLetters', array_keys($brandsByFirstLetter));
