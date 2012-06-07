5b3d83038a7516f8de77422e577746a805ecd6c6
Eric HAUSWALD <eric.hauswald@rbs.fr>
2012-06-07 09:15:36 +0200

    [FIX #59631] Indexation de documents via injection

--- persistentdocument/brand.class.php
+++ persistentdocument/brand.class.php
@@ -3,6 +3,22 @@
  * brand_persistentdocument_brand
  * @package modules.brand
  */
-class brand_persistentdocument_brand extends brand_persistentdocument_brandbase
+class brand_persistentdocument_brand extends brand_persistentdocument_brandbase implements indexer_IndexableDocument
 {
+	/**
+	 * Get the indexable document
+	 *
+	 * @return indexer_IndexedDocument
+	 */
+	public function getIndexedDocument()
+	{
+		$indexedDoc = new indexer_IndexedDocument();
+		$indexedDoc->setId($this->getId());
+		$indexedDoc->setDocumentModel('modules_brand/brand');
+		$indexedDoc->setLabel($this->getLabel());
+		$indexedDoc->setLang(RequestContext::getInstance()->getLang());
+		$text = f_util_StringUtils::htmlToText($this->getDescription(), false, true);
+		$indexedDoc->setText($text);
+		return $indexedDoc;
+	}
 }
